260e548296c9b7cee5f759c8e810fc57697d3be0
er.beeps <bipinkhatiwada10@gmail.com>
2022-02-23 11:24:51 +0545

    fixes

--- hmis_django/settings.py
+++ hmis_django/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hmis_django/static/'),
     os.path.join(BASE_DIR, 'adminlte/static/'),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
